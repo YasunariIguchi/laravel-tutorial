07eb667edf64cd6ceb04711717b06c54a2b39bbd
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2023-11-23 14:38:09 +0900

    (6) ToDoアプリのタスク作成機能を作る

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Folder;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -18,8 +19,30 @@ class TaskController extends Controller
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $current_folder -> $id,
+            'current_folder_id' => $current_folder -> id,
             'tasks' => $tasks,
+        ]);
+    }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
